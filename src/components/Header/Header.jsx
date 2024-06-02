e13204bdd82de85cb76b46b14227446d60e9332f
vikrugpro <vikrugpro@gmail.com>
2024-06-02 23:52:02 +0300

    added changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="header__text">Words to learn</h1>
+        </header>
+    );
+}
+export default Header;
